6c9fd7f90537d2c3ed3ec87d03f72f89cf7ba32c
knmyers <49408572+knmyers@users.noreply.github.com>
2020-04-19 00:56:37 -0700

    Update 10_mail.py

--- 10_mail.py
+++ 10_mail.py
@@ -1,21 +1,23 @@
 # 10_email_notifier.py
-# From the code for the Electronics Starter Kit for the Raspberry Pi by MonkMakes.com
+# adapted from the code for the Electronics Starter Kit for the Raspberry Pi by MonkMakes.com
 # Based on Recipe 7.15 in The Raspberry Pi Cookbook by Simon Monk.
 
 import RPi.GPIO as GPIO
 import smtplib, time            # smtp - Simple Mail Transport Protocol - library for sending email
 
 SMTP_SERVER = 'smtp.gmail.com'
+"TLS"
 SMTP_PORT = 587
 
 # Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
+
 GPIO.setmode(GPIO.BCM)
-red_pin = 18
-green_pin = 23
-switch_pin = 24
+red_pin = 17
+green_pin = 27
+switch_pin = 22
 
 GPIO.setup(red_pin, GPIO.OUT)
-GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(red_pin, GPIO.OUT)
 GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Use the smtp library to send an email 
@@ -32,23 +34,46 @@ def send_email(username, password, recipient, subject, text):
     smtpserver.sendmail(username, recipient, msg)
     smtpserver.close()
 
-# Utility function to turn the gree LED on and the red off
+# Utility function to turn the green LED on and the blue off
+#def green():
+    #GPIO.output(green_pin, True)
+    #GPIO.output(red_pin, False)
+
+# Utility function to turn the blue LED on and the green off
+#def red():
+    #GPIO.output(green_pin, False)
+    #GPIO.output(red_pin, True)
+
+#implemented the LED class from bink to control my LED pins"   
+class LED:
+     #state = False
+    
+    def __init__(self, start_state, id_pin):
+        self.state = start_state
+        self.pin = id_pin
+        GPIO.setup(self.pin, GPIO.OUT)
+    
+    def switch(self):
+        self.state = not self.state
+        GPIO.output(self.pin, self.state)
+            
+  #setting pins on the board      
+red_LED = LED(False,17)
+green_LED = LED(True,27)
+
+#rewrote the utility function using my LED class
 def green():
-    GPIO.output(green_pin, True)
-    GPIO.output(red_pin, False)
-
-# Utility function to turn the red LED on and the green off
+        red_LED.switch()
 def red():
-    GPIO.output(green_pin, False)
-    GPIO.output(red_pin, True)
-
+        green_LED.switch()
 # Prompt the user to enter their email details
 # Should only have to do this once each time the program is started
-username = raw_input("Sending gmail address? ")
-password = raw_input("Sending gmail password? ")
-recipient = raw_input("Send email to? ")
-subject = raw_input("Subject? ")
-message = raw_input("Message ? ")
+#had to update the code from python 2 to python3
+username = input("Sending gmail address? ")
+password = input("Sending gmail password? ")
+recipient = input("Send email to? ")
+subject = input("Subject? ")
+message = input("Message ? ")
 
 print("Press the button to send the Email")
 
@@ -59,4 +84,3 @@ while True:
         send_email(username, password, recipient, subject, message)
         time.sleep(3)
         print("Press the button to send the Email")
-        
